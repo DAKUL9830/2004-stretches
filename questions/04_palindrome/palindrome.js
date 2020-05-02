9a57fdd20290179aa21233a471bf4d8d01f29180
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-02 13:23:30 -0400

    palindrome

--- questions/04_palindrome/palindrome.js
+++ questions/04_palindrome/palindrome.js
@@ -1,8 +1,16 @@
 /* Given a string, determine if it is a palindrome */
 /* A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam, or racecar */
 
-const validPalindrome = () => {
-  //YOUR CODE
+const validPalindrome = (str) => {
+  //YOUR CODEs
+  if(str===str.split('').reverse.join('')){
+    return true
+  }else{
+      return false 
+    }
+
+    
+  
 };
 
 module.exports = { validPalindrome };
