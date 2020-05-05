0e1eeb5325e13b73166e3d45ddc1f10bcd117259
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-05 01:16:10 -0400

    updated

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,5 +1,44 @@
-const lockBox = () => {
+const lockBox = (elem1,elem2) => {
   // write code here
+  if(typeof elem1!=='number'||typeof elem2!=='string'){
+
+    throw Error
+  }
+  let code=elem1;
+  let message=elem2;
+  return{
+
+  modCode:(prevCode,newCode)=>{
+   if(prevCode!==code){
+     throw Error
+   }else{
+     code=newCode
+   }
+   return newCode;
+
+
+  },
+  modMessage:(prevMessage,newMessage)=>{
+    if(prevMessage!==message){
+      throw Error
+    
+    }else{
+      message=newMessage
+    }
+    return newMessage
+
+
+  },
+  revealMessage:(num,str)=>{
+    code=newCode
+    if(num!==newCode){
+        throw Error
+    }else{
+      return str
+    }
+
+  }
+  }
 };
 
 module.exports = { lockBox };
