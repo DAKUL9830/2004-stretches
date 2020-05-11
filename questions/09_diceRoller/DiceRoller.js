72f851b370a05250031da283f8a0e92baac0b61f
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-11 18:55:40 -0400

    fizzBuzz

--- questions/09_diceRoller/DiceRoller.js
+++ questions/09_diceRoller/DiceRoller.js
@@ -3,7 +3,30 @@
 // history property which outputs all of the previous rolls
 
 class DiceRoller {
+  constructor(sides,numDice){
+    this.sides=sides;
+    this.numDice=numDice;
+    this.history=[]
+
+    if(typeof sides!=='number'||sides<=0){
+      throw Error;
+    }
+    else if(typeof numDices!=='number'||numDice<=0){
+      throw Error;
+    }
+  }
   //YOUR CODE
+  
+  roll(sides,numDice){
+    let sidesArr=[]
+    sidesArr.push(sides);
+    let numArr=[]
+    numArr.push(numDice);
+    let result ={sidesArr,numArr}
+    this.history.push(result);
+    return this.history;
+
+  }
 }
 
 module.exports = { DiceRoller };
