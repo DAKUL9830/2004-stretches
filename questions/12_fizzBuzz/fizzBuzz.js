06f77653e0e0815edb77b38cc4342978c975b5d4
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-11 21:52:16 -0400

    updated

--- questions/12_fizzBuzz/fizzBuzz.js
+++ questions/12_fizzBuzz/fizzBuzz.js
@@ -1,4 +1,20 @@
-const fizzBuzz = () => {
+const fizzBuzz = (num) => {
   //YOUR CODE HERE
+  if(typeof num==='number'||typeof num<0){
+   throw Error;
+  }
+  for(let i=0;i<num;i++){
+    if(i%3===0){
+      return 'Fizz'
+    } if(i%5){
+      return 'Buzz'
+    }
+    else if(i%15){
+      return 'FizzBuzz'
+    }
+
+  }
+  //started too late
+
 };
 module.exports = { fizzBuzz };
