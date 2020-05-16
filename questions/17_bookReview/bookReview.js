e8b25506e49f15a692a7ab676fef03b00094d9df
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-16 16:09:20 -0400

    updated

--- questions/17_bookReview/bookReview.js
+++ questions/17_bookReview/bookReview.js
@@ -1,2 +1,31 @@
-class BookReview {}
+class BookReview {
+constructor(title,author){
+    this.title=title;
+    this.author=author
+    this.notes=[];
+    this.rating=0
+}
+addNotes(page,note){
+    if(this.notes[page]){
+        this.notes[page]=this.notes[page]+' '+note;
+    }else{
+        this.notes[page]=note;
+    }
+   
+    }
+
+
+addRating(num){
+    if(num<0||num>5){
+        throw new Error
+    }else{
+        this.rating=num;
+    }
+
+
+
+}
+
+
+}
 module.exports = { BookReview };
