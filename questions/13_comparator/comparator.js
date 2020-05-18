b812eb05a456aa9f8586f11e510195a9adb5d55c
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-18 02:38:45 -0400

    updated

--- questions/13_comparator/comparator.js
+++ questions/13_comparator/comparator.js
@@ -1,12 +1,21 @@
 //write a func that takes two arg... selectKey and obj
 
-const comparator = (obj) => {
-    const newObj={};
+const comparator = (selectKey,Obj) => {
+    //const newObj={};
 
- Object.keys(obj).sort().forEach(function(key){
+/* Object.keys(obj).sort().forEach(function(key){
      newObj[key]=obj[key];
  })
- return newObj
+ return newObj;*/
+ if(!selectKey){
+     throw new Error("incorrect key")
+ }
+ return Object.keys(Obj).sort().reduce(acc,key=>{
+      if(acc[key]===obj[key]){
+          return acc;
+      }
+ },{})
+
 };
 
 
