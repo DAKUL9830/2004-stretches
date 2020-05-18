d5e6061fc8fcd348f52639a4d6b19238c76b9893
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-18 02:26:24 -0400

    comparator

--- questions/13_comparator/comparator.js
+++ questions/13_comparator/comparator.js
@@ -1,5 +1,13 @@
 //write a func that takes two arg... selectKey and obj
 
-const comparator = () => {};
+const comparator = (obj) => {
+    const newObj={};
+
+ Object.keys(obj).sort().forEach(function(key){
+     newObj[key]=obj[key];
+ })
+ return newObj
+};
+
 
 module.exports = { comparator };
