297a166cb2229fa5ff015341db6436f7a7ebc8b3
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-30 09:20:52 -0400

    btown

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,5 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (func) => {};
 
 module.exports = { extensions };
