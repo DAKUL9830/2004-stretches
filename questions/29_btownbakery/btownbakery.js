d90e2101c804d353cec19d5cbcf3f4b031d15211
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-30 15:00:01 -0400

    updated'

--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,6 +1,20 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = () => {
+const btownbake = (recipe,supplies) => {
+  
+  if(typeof recipe!=='object'||typeof supplies!=="object"){
+    throw new Error("Thisis not an object")
+  }
+  let newNum=[];
+ for(let key in recipe){
+   if(key in supplies){
+      newNum.push(Math.floor(supplies[key]/recipe[key]));
+   }else{
+     return 0;
+   }
+   
+ }
+   return Math.min(...newNum);
   //enter code here
 };
 
